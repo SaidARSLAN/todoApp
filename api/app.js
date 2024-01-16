ce584b9a1dcd936036b819b506ca1528df174061
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-16 22:44:39 +0300

    Client and Server connection has been established

--- api/app.js
+++ api/app.js
@@ -3,7 +3,18 @@ const PORT_NUMBER = 3434
 const app = express()
 
 
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin","*"),
+    res.setHeader("Access-Control-Allow-Methods","*")
+    next()
+})
 
+
+app.get("/todos",(request,response) => {
+
+    response.send("Todos are showing...")
+
+})
 
 
 app.listen(PORT_NUMBER, (req,res) => {
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,8 +4,19 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './styles/reset.css'
+import { useEffect } from "react";
 function App() {
 
+  const getTodos = () => {
+    return fetch("http://localhost:3434/todos")
+    .then(result => result)
+    .catch(err => console.error(err))
+  }
+  useEffect(() => {
+    getTodos()
+    .then(data => data.text())
+    .then(result => console.log(result))
+  },[])
   return (
     <main className="main-color">
     <Container className="pt-5">
