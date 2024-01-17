c894a23541747d9470289990cd9658392bc4dc4d
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-17 22:42:28 +0300

    Added api and ui

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -7,16 +7,7 @@ import './styles/reset.css'
 import { useEffect } from "react";
 function App() {
 
-  const getTodos = () => {
-    return fetch("http://localhost:3434/todos")
-    .then(result => result)
-    .catch(err => console.error(err))
-  }
-  useEffect(() => {
-    getTodos()
-    .then(data => data.text())
-    .then(result => console.log(result))
-  },[])
+  
   return (
     <main className="main-color">
     <Container className="pt-5">
--- web/src/components/CreateTodo.jsx
+++ web/src/components/CreateTodo.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.css';
 import Form from 'react-bootstrap/Form'
+import axios from 'axios'
 
 const CreateTodo = () => {
+
+    const [id,setId] = useState(null)
+    const [title,setTitle] = useState("")
+    const [description,setDescription] = useState("")
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3434/todos',
+      {
+        headers : {
+          "Access-Control-Allow-Origin": "*"
+        },
+        id : 0,
+        title : title,
+        description : description,
+        isCompleted : false
+      })
+      .then(result => {
+        if (result.status === 200) {
+          console.log("Todo has been sended",result.data)
+          setTitle("")
+          setDescription("")
+        } 
+      })
+      .catch(err => console.error(err))
+    }
+
   return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Todo Title</Form.Label>
-        <Form.Control type="email" placeholder="Add Todo..." />
+        <Form.Control 
+        type="text"
+        placeholder="Add Todo..."
+        name='title'
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>Todo Description</Form.Label>
-        <Form.Control as="textarea" rows={4} />
+        <Form.Control
+        as="textarea"
+        rows={4}
+        name='description'
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+        />
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
       <Button variant="primary" type="submit">
         Submit
       </Button>
