13ac497fcc2bb1119fff4c77c06eab426a401b76
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-17 21:09:18 +0300

    Added mongodb connection and mongoose schemas

--- api/app.js
+++ api/app.js
@@ -1,14 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const PORT_NUMBER = 3434
+
+const username = "admin"
+const password = "roottoor"
+const ip = "localhost"
+const db_port = "27017"
+const database = "admin"
+
+const URL = `mongodb://${username}:${password}@${ip}:${db_port}/?authSource=${database}`
+
 const app = express()
 
-
+app.use(express.json)
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin","*"),
     res.setHeader("Access-Control-Allow-Methods","*")
     next()
 })
 
+mongoose.connect(URL)
+.then(() => console.log("Mongodb server has been established!"))
+.catch(err => console.log("Database error:",err))
 
 app.get("/todos",(request,response) => {
 
---
+++ api/models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const todoSchema = mongoose.Schema({
+
+    id : Number,
+    title : String,
+    description : String,
+    isCompleted : Boolean,
+
+})
+
+const Todo = mongoose.model("Todo",todoSchema)
+
+module.exports = {todoSchema}
