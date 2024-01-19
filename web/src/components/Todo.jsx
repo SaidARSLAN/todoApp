d92f745f74c4934584c05d6a46dc54601132b178
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-19 22:11:15 +0300

    Added react-modal for ui edit

--- web/src/components/Todo.jsx
+++ web/src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack';
 import GlobalContext from '../context/MainContext';
@@ -6,15 +6,19 @@ import GlobalContext from '../context/MainContext';
 const Todo = ({todo,idx}) => {
 
   const {deleteTodo} = useContext(GlobalContext)
-  
+  const [isModalOpen, setIsOpenModal] = useState(false)
 
+    const openModal = () => {
+      setIsOpenModal(true)
+
+    }
   return (
     <tr>
       <td>{idx}</td>
       <td>{todo.title}</td>
       <td>{todo.description}</td>
       <Stack direction='horizontal' gap={3}>
-        <Button className='ms-auto'>Edit</Button>
+        <Button className='ms-auto' onClick={openModal}>Edit</Button>
         <Button variant='danger' onClick={() => deleteTodo(todo.id)}>Delete</Button>
       </Stack>
     </tr>
