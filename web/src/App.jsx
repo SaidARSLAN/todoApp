c8983d257a5313d5b16f019f29197ec78cabf913
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-21 12:57:47 +0300

    Added main page route

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,19 +1,21 @@
-import { Route, Router, Routes } from 'react-router-dom'
+import { Link, Route, Router, Routes } from 'react-router-dom'
 import './styles/reset.css'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import MainPage from './pages/MainPage'
+
 function App() {
 
   
   return (
     <main className="main-color">
       <Routes>
+        <Route path='/' element={<MainPage />} />
           <Route path='/login' element={<Login />} />
           <Route path='/profile' element={<Profile />}/>
           <Route path='/register' element={<Register />} />
       </Routes>
-
     
     </main>
   )
---
+++ web/src/pages/MainPage.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/esm/Col'
+import { Link } from 'react-router-dom'
+
+const MainPage = () => {
+
+
+    return (
+        <Container className='main-page'>
+        <Row>
+            <Col>
+          <Link to='/login'  className='btn btn-primary'>Login</Link>
+            </Col>        
+          </Row>
+            <Row>
+                <Col>
+                <Link to='/register' className='btn btn-primary'>Register</Link>
+
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+          <Link to='/profile' className='btn btn-primary'>See Todos</Link>
+                </Col>
+            </Row>
+      </Container>
+    )
+}
+export default MainPage
